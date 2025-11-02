e3a1ae91c1872a1a7a718b2e7d6281df1117393f
hasan rami aydin <72613302+hasanraydin@users.noreply.github.com>
2025-11-02 23:18:46 +0100

    Update the car.alpha_u parameter value from 1.35 to 2.00

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/parameters/IDFModeParameters.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/parameters/IDFModeParameters.java
@@ -26,7 +26,7 @@ public class IDFModeParameters extends ModeParameters {
 		parameters.referenceEuclideanDistance_km = 40.0;			//base value = 40.0
 
 		// Car
-		parameters.car.alpha_u = 1.35;								//base value = 1.35
+		parameters.car.alpha_u = 2.00;								//base value = 1.35
 		parameters.car.betaTravelTime_u_min = -0.06;				//base value = -0.06
 
 		parameters.car.additionalAccessEgressWalkTime_min = 4.0;	//base value = 4.0
