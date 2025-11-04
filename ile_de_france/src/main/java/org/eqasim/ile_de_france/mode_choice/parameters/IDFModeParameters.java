4a937c3e9337b2c26d7e0106176c26116892c9df
hasan rami aydin <72613302+hasanraydin@users.noreply.github.com>
2025-11-04 13:16:57 +0100

    Update the drt.betaWaitingTime_u_min parameter value from -0.1 to 1.0

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/parameters/IDFModeParameters.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/mode_choice/parameters/IDFModeParameters.java
@@ -37,8 +37,8 @@ public class IDFModeParameters extends ModeParameters {
 
 		// Drt
 		parameters.drt.alpha_u = 0.0;								//base value = 0.0
-		parameters.drt.betaTravelTime_u_min = 1.0;					//base value = -0.2
-		parameters.drt.betaWaitingTime_u_min = -0.1;				//base value = -0.1
+		parameters.drt.betaTravelTime_u_min = -0.2;					//base value = -0.2
+		parameters.drt.betaWaitingTime_u_min = 1.0;				//base value = -0.1
 		parameters.drt.betaAccessEgressTime_u_min = -0.0804;		//base value = -0.0804
 
 		// PT
