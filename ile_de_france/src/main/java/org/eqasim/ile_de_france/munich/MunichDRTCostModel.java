dc65647d4faff58151636d75dc0a2e1027f3cfad
hasan rami aydin <72613302+hasanraydin@users.noreply.github.com>
2026-02-06 14:46:26 +0100

    Rename ZeroCostModel to MunichDRTCostModel

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/munich/MunichDRTCostModel.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/munich/MunichDRTCostModel.java
@@ -6,7 +6,7 @@ import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
 import org.matsim.contribs.discrete_mode_choice.model.DiscreteModeChoiceTrip;
 
-public class ZeroCostModel implements CostModel {
+public class MunichDRTCostModel implements CostModel {
 	@Override
 	public double calculateCost_MU(Person person, DiscreteModeChoiceTrip trip, List<? extends PlanElement> elements) {
 		return 0.0;
